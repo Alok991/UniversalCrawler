c603894d0161443b6084086599d1558fa3aaba28
Alok Dixit <alokdixit.iitkgp@gmail.com>
2018-11-28 23:03:20 +0530

    a sample card-data structure for making it generic

---
+++ config.py
@@ -0,0 +1,23 @@
+structure = {
+
+    "class":"feed-carousel-shelf",
+    "child":[
+        {
+            "class":"feed-carousel-card",
+            "child":[
+                {
+                    "tag":"a"
+                }
+            ]
+        },
+        {
+            "class":"deals-shoveler-card-bottom",
+            "child":[
+                {
+                    "class":"dealPrice"
+                }
+            ]
+        }
+    ]
+
+}
